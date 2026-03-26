c0a1dd6d73d9a4d27d49125d627617f74e93d0ec
Abdou BA <ba2664890@gamil.com>
2026-03-26 13:14:43 -0400

    ajout des fichiers modif

--- sakinafinance/compliance/urls.py
+++ sakinafinance/compliance/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.compliance_view, name='compliance_dashboard'),
+    path('', views.compliance_view, name='taxation'),
     path('api/data/', views.api_compliance_data, name='api_compliance_data'),
     path('tax/create/', views.tax_filing_create, name='tax_filing_create'),
     path('risks/add/', views.compliance_risk_create, name='compliance_risk_create'),
