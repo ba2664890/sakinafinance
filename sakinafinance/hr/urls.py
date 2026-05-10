790a68be6fd86f0c602c19336c035394feccc849
Ton Nom <ton.email@exemple.com>
2026-05-10 21:28:27 +0000

    correction de formulaire

--- sakinafinance/hr/urls.py
+++ sakinafinance/hr/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.hr_dashboard, name='hr_payroll'),
     path('api/data/', views.api_hr_data, name='api_hr_data'),
+    path('employees/', views.employee_list, name='employee_list'),
     path('employees/create/', views.employee_create, name='employee_create'),
     path('employees/<uuid:pk>/', views.employee_detail, name='employee_detail'),
     path('leaves/add/', views.leave_request_create, name='leave_request_create'),
